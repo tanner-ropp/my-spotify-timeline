1ee1531540fabdf24d215a86111aaeb3736389a3
Tanner Ropp <tanner.ropp@ufl.edu>
2020-08-12 14:00:35 -0400

    added infinite scroll

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Spotify from 'spotify-web-api-js'
+import ReleaseCard from './components/ReleaseCard.js'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const spotifyWebApi = new Spotify();
+//const calendar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//const today = new Date();
 
 class App extends Component {
   constructor(props) {
@@ -13,82 +16,70 @@ class App extends Component {
     this.state = {
         loggedIn : params.access_token ? true : false,
         artists : [], // just artists
-        artistAlbums : [], // albums grouped by artist,
+        //artistAlbums : [], // albums grouped by artist,
         artistReleases : [],
+        loadedReleases : [],
         userDataReceived : false, // used to control rendering,
         displayName: "",
-        profileImage: ""
+        profileImage: "",
+        hasMore: true
     }
 
     if (params.access_token) {
         spotifyWebApi.setAccessToken(params.access_token);
+        console.log("hmm")
     }
 
+    console.log("constructor");
+    this.fetchData = this.fetchData.bind(this);
   }
 
   componentDidMount() {
-      /*if (this.state.loggedIn) {
-          spotifyWebApi.getFollowedArtists({limit:50}) // WHAT IF YOU HAVE MORE THAN 50 FOLLOWED ARTISTS?
-          .then((response) => { // waits for artists followed
-              this.setState({
-                  artists : response.artists.items
-              });
-              return response.artists.items.map((artist) => {return {name : artist.name, id : artist.id}});
-          })
-          .then((artists) => { // waits for artist id array
-              const artistAlbums = artists.map((artist) => {
-                  return spotifyWebApi.getArtistAlbums(artist.id, {limit:10, country:'US'});
-              });
-              return Promise.all(artistAlbums); // this resolves the promise for each album request
-          })
-          .then((albums) => {
-              console.log(albums);
-              this.setState({
-                  artistAlbums: albums,
-                  userDataReceived : true
-              });
-          });
-      }*/
-      //spotifyWebApi.play({});
-
       if (this.state.loggedIn) {
+          console.log("logged in did mount")
           spotifyWebApi.getMe().then((response) => {this.setState({displayName: response.display_name, profileImage: response.images[0].url})})
 
-          spotifyWebApi.getFollowedArtists({limit:20}) // WHAT IF YOU HAVE MORE THAN 50 FOLLOWED ARTISTS?
+          spotifyWebApi.getFollowedArtists({limit:50}) // WHAT IF YOU HAVE MORE THAN 50 FOLLOWED ARTISTS?
           .then((response) => { // waits for artists followed
-              this.setState({
+              /*this.setState({
                   artists : response.artists.items
-              });
+              });*/
               return response.artists.items.map((artist) => {return {name : artist.name, id : artist.id}});
           })
           .then((artists) => { // waits for artist id array
               const artistAlbums = artists.map((artist) => {
-                  return spotifyWebApi.getArtistAlbums(artist.id, {limit:10, country:'US'})
+                  return spotifyWebApi.getArtistAlbums(artist.id, {limit:50, country:'US', include_groups: 'album,single'})
                   .then((albums) => {
-                      //console.log(albums.items);
                       return (albums.items);
                   })
               });
               return Promise.all(artistAlbums);
-              //return Promise.all(artistAlbums); // this resolves the promise for each album request
           })
           .then((albums) => {
-              //console.log(albums.flat());
-
+              const releases = albums.flat().sort((a,b) => a.release_date < b.release_date ? 1 : -1);
               this.setState({
-                  artistReleases : albums.flat().sort((a,b) => a.release_date < b.release_date),
-                  artistAlbums: albums,
+                  artistReleases : releases, // hack for chrome functionality
+                  loadedReleases : releases.slice(0,10),
+                  //artistAlbums: albums,
                   userDataReceived : true
               });
           });
       }
   }
 
+  fetchData() {
+      const oldLength = this.state.loadedReleases.length;
+      this.setState({
+          loadedReleases : this.state.loadedReleases.concat(this.state.artistReleases.slice(oldLength, oldLength + 10))
+      })
+      console.log("FETCH");
+  }
+
   getHashParams() { // provided by jmperez
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
+    while ( e === r.exec(q)) {
        hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
@@ -96,20 +87,20 @@ class App extends Component {
 
   render() {
       //const artistsFollowed = this.state.artists;
+      console.log("app rendered");
 
-    const artistReleases = this.state.artistReleases;
+    /*const artistReleases = this.state.artistReleases;
     const releaseList = artistReleases.map((release)=> {
         return (
-            <div key={release.id} style={{
-            border:'1px solid charcoal',
-            margin: '4px',
-            padding: '5px'
-            }}>
-                <div>{release.name}</div>
-                <img src={release.images[0].url} height="200" onClick={() => spotifyWebApi.play({context_uri : release.uri})}></img>
-                <div>{release.release_date}</div>
-            </div>
-        )
+            <ReleaseCard key={release.id} releaseInfo={release}/>
+        );
+    })*/
+
+    const loadedReleases = this.state.loadedReleases;
+    const releaseList = loadedReleases.map((release)=> {
+        return (
+            <ReleaseCard key={release.id} releaseInfo={release}/>
+        );
     })
 
       return (
@@ -128,7 +119,19 @@ class App extends Component {
                         <button style={{background: '#1DB954'}}>Login to Spotify</button>
                     </a>
                 }
-                {releaseList}
+                <InfiniteScroll
+                  dataLength={this.state.loadedReleases.length} //This is important field to render the next data
+                  next={this.fetchData}
+                  hasMore={this.state.hasMore}
+                  loader={<h4>Loading...</h4>}
+                  endMessage={
+                    <p style={{textAlign: 'center'}}>
+                      <b>Yay! You have seen it all</b>
+                    </p>
+                  }>
+                  {releaseList}
+                </InfiniteScroll>
+                {/*releaseList*/}
             </div>
         </div>
       );
---
+++ client/src/components/ReleaseCard.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import Spotify from 'spotify-web-api-js'
+
+
+const spotifyWebApi = new Spotify();
+const calendar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const today = new Date();
+
+class ReleaseCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        tracks : []
+    }
+  }
+
+  componentDidMount() {
+      const release = this.props.releaseInfo;
+      spotifyWebApi.getAlbum(release.id)
+      .then((fullRelease) => {
+          this.setState({
+              tracks : fullRelease.tracks.items
+          })
+      });
+  }
+
+  render() {
+    const release = this.props.releaseInfo;
+    const releaseDate = new Date(release.release_date);
+
+    var displayedDate = '';
+    if (releaseDate.getFullYear() === today.getFullYear()) {
+        displayedDate = displayedDate.concat(calendar[releaseDate.getMonth()]);
+        displayedDate = displayedDate.concat(' ' + releaseDate.getDate() + ', ');
+    }
+    displayedDate = displayedDate.concat(releaseDate.getFullYear());
+
+    const trackList = this.state.tracks.map((track) => {
+        return (
+            <div key={track.id} className="release-playbar">{track.name}</div>
+        );
+    })
+
+      return (
+          <div key={release.id} className="release-card">
+              <div><img src={release.images[0].url} height="200" onClick={() => spotifyWebApi.play({context_uri : release.uri})}></img></div>
+              <div className="release-card-body">
+                  <div>{displayedDate}</div>
+                  <h2>{release.name}</h2>
+                  <h3>by {release.artists[0].name}</h3>
+              </div>
+              <div className="release-tracklist">{trackList}</div>
+          </div>
+      );
+  }
+}
+
+export default ReleaseCard;
